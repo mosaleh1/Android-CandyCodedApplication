edd0ca51cdc9e22e07956ffd21ec940f629ae495
Mohamed Saleh <MohamedSalehOfficial@gmail.com>
2020-09-25 11:38:30 +0200

    test 1 and 2  solved android candy store

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -1,5 +1,6 @@
 package com.pluralsight.candycoded;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -23,11 +24,19 @@ public class InfoActivity extends AppCompatActivity {
 
     }
 
-    // ***
-    // TODO - Task 2 - Launch the Google Maps Activity
-    // ***
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // ***
+        // TODO - Task 2 - Launch the Google Maps Activity
+        // ***
+        Uri mapsUri = Uri.parse("google.streetview:cbll=46.414382,10.013988");
+        Intent intent = new Intent(Intent.ACTION_VIEW,mapsUri);
+        startActivity(intent);
+        // ***
+        // TODO - Task 3 - Launch the Phone Activity
+        // ***
+    }
 
-    // ***
-    // TODO - Task 3 - Launch the Phone Activity
-    // ***
+
 }
